5413b2e055e411f54f3d4788409f94c12f0738ee
Jonathan Sick <jonathansick@mac.com>
2014-12-19 13:53:37 -0500

    Parse the optical fit header

--- magphysio/readers.py
+++ magphysio/readers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-Python representation of MAGPHYS result files.
+Python readers/representation of MAGPHYS result files.
 
 2014-12-17 - Created by Jonathan Sick
 """
@@ -9,7 +9,54 @@ Python representation of MAGPHYS result files.
 import numpy as np
 
 
-class OpticalFit(object):
+class BaseReader(object):
+    def __init__(self):
+        super(BaseReader, self).__init__()
+
+    @staticmethod
+    def _parse_observed_sed(lines, index=1):
+        bands = lines[index].replace('#', '').strip().split()
+        sed = np.array(map(float, lines[index + 1].strip().split()))
+        err = np.array(map(float, lines[index + 2].strip().split()))
+        return bands, sed, err
+
+    @staticmethod
+    def _parse_model_sed(lines, index=11):
+        bands = lines[index].replace('#', '').strip().split()
+        sed = np.array(map(float, lines[index + 1].strip().split()))
+        return bands, sed
+
+    @staticmethod
+    def _parse_best_fit(lines, index=8):
+        parts = lines[index].strip().split()
+        sfh_i = int(parts[0])
+        chi2 = float(parts[1])
+        z = float(parts[2])
+        return sfh_i, chi2, z
+
+    @staticmethod
+    def _parse_pdf(lines, start_n, percentile_n):
+        end_n = percentile_n - 1  # line where the PDF grid ends
+        n_bins = end_n - start_n
+        bins = np.empty(n_bins, dtype=np.float)
+        probs = np.empty(n_bins, dtype=np.float)
+        print(lines[start_n - 1])
+        for i, line in enumerate(lines[start_n:end_n]):
+            b, p = map(float, line.strip().split())
+            bins[i] = b
+            probs[i] = p
+        percentiles = map(float, lines[percentile_n].strip().split())
+        d = {"bins": bins,
+             "probs": probs,
+             "2.5": percentiles[0],
+             "16": percentiles[1],
+             "50": percentiles[2],
+             "84": percentiles[3],
+             "97.5": percentiles[4]}
+        return d
+
+
+class OpticalFit(BaseReader):
     """A MAGPHYS model fit for Roediger's fit_magphys_opt.exe mod."""
     def __init__(self, galaxy_id, fit_path):
         super(OpticalFit, self).__init__()
@@ -18,6 +65,10 @@ class OpticalFit(object):
 
         with open(fit_path) as fit_file:
             fit_lines = fit_file.readlines()
+        self.bands, self.sed, self.sed_err = OpticalFit._parse_observed_sed(
+            fit_lines)
+        _, self.model_sed = OpticalFit._parse_model_sed(fit_lines)
+        self.i_sfh, self.chi2, self.z = OpticalFit._parse_best_fit(fit_lines)
         self._pdfs['Z/Zo'] = OpticalFit._parse_pdf(fit_lines, 16, 120)
         self._pdfs['tform'] = OpticalFit._parse_pdf(fit_lines, 122, 258)
         self._pdfs['gamma'] = OpticalFit._parse_pdf(fit_lines, 260, 361)
